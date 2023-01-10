4cfbc1fb3e3fed00ea4a69943eff3ae29c0bcefd
Aaditya <120321162+Adi-vanii@users.noreply.github.com>
2023-01-10 19:34:43 +0530

    Add files via upload

---
+++ right_triangle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+    int i ,k;
+    for ( i = 1; i <10; i++)
+    {
+        for ( k = 1; k <= i; k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
