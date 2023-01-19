2efed9f9b779dc36d3dff351e46ca291af2fe83d
Aaditya <120321162+Adi-vanii@users.noreply.github.com>
2023-01-19 19:36:46 +0530

    Add files via upload

---
+++ two_d_arrey.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+    int a[5][3], i , j ;
+    printf("enter the value\n");
+    for ( i = 0; i < 5; i++)
+    {
+        for ( j = 0; j < 3; j++)
+        {
+            scanf("%d", a[i][j]);
+        }
+        
+    }
+    
+}
